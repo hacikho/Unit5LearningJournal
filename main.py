c209996351479027dc78c9338eaf66a8d922081a
Haci Karahasanoglu <haci@hacikho.com>
2020-12-19 23:40:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+
+for letter in prefixes:
+    if(letter == 'O' or letter == 'Q'):
+      print(letter + 'u' + suffix)
+    else:
+      print(letter + suffix)
+
+
+
+
+for letter in prefixes:
+    if(letter == 'O' or letter == 'Q'):
+      print(letter + 'u' + suffix)
+    else:
+      print(letter + suffix)
